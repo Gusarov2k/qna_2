541763bf1a62ad6f57a6a33e3fac5184d1088635
gusarov2k <motoface@yandex.ru>
2018-05-27 18:55:47 +0300

    refactoring rspect
    time to lesson 38 min

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
 	describe 'GET #index' do
 		
+		let(:questions) {FactoryBot.create_list(:question, 2)}
+
 		before do
-			@questions = FactoryBot.create_list(:question, 2)
 			get :index
 		end
 
 
 		it 'populates an array of all questions' do
-			expect(assigns(:questions)).to match_array(@questions)
+			expect(assigns(:questions)).to match_array(questions)
 		end
 		
 		it 'renders index view' do
