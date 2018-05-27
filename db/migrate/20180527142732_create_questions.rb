cf7b6495594f3389e9a62260c834fc97c4d2137f
gusarov2k <motoface@yandex.ru>
2018-05-27 17:38:02 +0300

    first test with rspec to validate

--- db/migrate/20180527142732_create_questions.rb
+++ db/migrate/20180527142732_create_questions.rb
@@ -1,6 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.string :title
+      t.text :body
 
       t.timestamps null: false
     end
